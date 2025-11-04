66399f3280898a3b926552e1929738242b325c89
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-11-05 00:34:45 +0100

    adds: more placeholders for options + toga fix PR in the venv (not included)

--- src/beebeeware/app.py
+++ src/beebeeware/app.py
@@ -4,6 +4,8 @@ An app for Bee.
 
 import asyncio
 import toga
+from decimal import Decimal
+
 
 from toga.style.pack import CENTER, COLUMN, ROW, Pack
 from toga.colors import WHITE, rgb
@@ -19,6 +21,8 @@ from typing import OrderedDict
 class BeeBeeware(toga.App):
 
     placeholder_text = "Placeholder"
+    main_window_split = {"menu": 1, "previews": 2}
+    preview_container_split = {"menu": 1, "options": 1}
 
     def startup(self):
         """Construct and show the Toga application.
@@ -27,11 +31,17 @@ class BeeBeeware(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        self.buttons = OrderedDict({"Placeholder button": self.draw_text})
-        main_box = toga.Box()
 
-        menu_previews_split = toga.SplitContainer()
-        menu = toga.Box(style=Pack(direction=COLUMN, margin_top=50))
+        self.main_window = toga.MainWindow(title=self.formal_name)
+
+        self.buttons = OrderedDict({
+            "Model": self.preview_model_menu,
+            "Config": self.preview_config})
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        menu_previews_split = toga.SplitContainer(style=Pack(direction=COLUMN))
+
+        menu = toga.Box(id="menu", style=Pack(direction=COLUMN, alignment=CENTER))
 
         for button_name, button_action in self.buttons.items():
             menu.add(
@@ -42,29 +52,66 @@ class BeeBeeware(toga.App):
                 )
             )
 
-        self.previews = toga.Box(style=Pack(direction=COLUMN, margin_top=50))
+        previews = toga.Box(style=Pack(direction=COLUMN, alignment=CENTER))
+        self.previews_container = toga.ScrollContainer(id="previews_container", horizontal=False, style=Pack(direction=COLUMN))
+        self.previews_container.content = previews
 
-        menu_previews_split.content = [(menu, 1), (self.previews, 1)]
+        menu_previews_split.content = [(menu, self.main_window_split["menu"]), (self.previews_container, self.main_window_split["previews"])]
 
-        main_box.add(menu_previews_split)
+        # main_box.add(menu_previews_split)
 
         self.canvas = toga.Canvas(
-            style=Pack(flex=1),
+            style=Pack(flex=1, direction=ROW),
             on_resize=self.on_resize,
             on_press=self.on_press,
+            alignment=CENTER,
         )
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = menu_previews_split
         self.main_window.show()
 
+    def preview_model_menu(self, widget) -> None:
+        base_models_data = [f"Base palceholder {index}" for index in range(2)]
+        lora_models_data = [f"lora placeholder {index}" for index in range(4)]
+
+        base_models_table = toga.Table(headings=["Base models"], data=base_models_data)
+        lora_models_table = toga.Table(headings=["Trainable model"], data=lora_models_data)
+        models = toga.SplitContainer(id="models", style=Pack(direction=COLUMN))
+        models.content = [(base_models_table, self.preview_container_split["menu"]), (lora_models_table, self.preview_container_split["options"])]
+        self.previews_container.content = models
+
+    def preview_config(self, widget):
+
+        config = OrderedDict({
+            "Placeholder 1": toga.NumberInput(min=0, max=10, step=0.1, value=1),
+            "Placeholder 2": toga.NumberInput(min=0, max=10, step=0.1, value=1)
+        })
+
+        config_scroll = toga.Box(id="config", style=Pack(direction=COLUMN))
+
+        for config_name, config_input in config.items():
+            label = toga.Label(config_name)
+            config_box = toga.Box(style=Pack(direction=ROW))
+
+            config_box.add(label)
+            config_box.add(config_input)
+
+            config_scroll.add(config_box)
+
+        self.previews_container.content = config_scroll
+
     def draw_text(self, widget):
-        self.previews.content = self.canvas
+        print("Writing on canvas.")
+        self.previews_container.content = self.canvas
         font = toga.Font(family=SANS_SERIF, size=20)
         self.text_width, text_height = self.canvas.measure_text(self.placeholder_text, font)
 
+        # print(f"{self.main_window.size.width=}")
+
         x = (150 - self.text_width) // 2
-        y = 175
+        y = 10
+
+        self.canvas.context.clear()
 
         with self.canvas.Stroke(color="REBECCAPURPLE", line_width=4.0) as rect_stroker:
             self.text_border = rect_stroker.rect(
@@ -75,6 +122,9 @@ class BeeBeeware(toga.App):
             )
         with self.canvas.Fill(color=rgb(149, 119, 73)) as text_filler:
             self.text = text_filler.write_text(self.placeholder_text, x, y, font, Baseline.TOP)
+
+
+        # self.previews_container.content.redraw()
 
     def on_resize(self, widget, width, height, **kwargs):
         # On resize, center the text horizontally on the canvas. on_resize will be
